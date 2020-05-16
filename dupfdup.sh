36bd0878c72bcb126a36b6df1ab72f5dcffa3b67
cbxcube <cub.bed@gmail.com>
2020-05-16 12:44:22 +0200

    done for a day

--- dupfdup.sh
+++ dupfdup.sh
@@ -2,9 +2,13 @@
 # find duplicates using fdupe binary
 
 targetdir=$1
+spacebefore=$(du -xms $targetdir)
+resultsfile=$targetdir$targetdir_duplicates-all.txt
+resultsfileomitted=$targetdir$targetdir_duplicates-ommited.txt
 
 if [ $# -gt 0 ]; then
 	echo "Starting to look for duplicates in directory: $targetdir - $(date)."
+	echo "Size of target: $targetdir - $spacebefore."
 else
 	echo "Target directory for duplicate search not provided. Exiting..."
 	exit 1
@@ -19,26 +23,75 @@ else
 
 fi
 
-search() {
-	if [ -ne "$targetdir"/"$targetdir"_dupplicates-all.txt ]; then
-		touch "$targetdir"/"$targetdir"_dupplicates-all.txt 
-		ls -l "$targetdir"/"$targetdir"_dupplicates-all.txt 
-		echo "Duplicates will be listed in file: "$targetdir"/"$targetdir"_dupplicates-all.txt"
 
-	elseif 
-		[ -e "$targetdir"/"$targetdir"_dupplicates-all.txt ]; then
-		echo "Duplicates result file "$targetdir"/"$targetdir"_dupplicates-all.txt already exist."
-		echo "Creating Result file in ~/"$targetdir"_dupplicates-all.txt"
+
+
+createresults() {
+
+	if	[ -f  $resultsfile ]; then
+		resultsfile2="~$targetdir_duplicates-all.txt"
+		echo "File for duplicates result $resultsfile2 already exist."
+		echo "Creating Result file in user's home $resultsfile2"
+		touch $resultsfile2
+		ls -l $resultsfile2
+		resultsfile=$resultsfile2
+
+	elif 	[ ! -f $resultsfile ]; then
+		touch $resultsfile 
+		ls -l $resultsfile 
+		echo "Duplicates will be listed in file: $resultsfile." 
+
 	else
-		echo "Failed to create the file: "$targetdir"/"$targetdir"_dupplicates-all.txt" 
+		echo "Failed to create the file: $resultsfile"
 		exit 1
 	fi
 
-
-	fdupes -r $targetdir > "$targetdir/$targetdir_dupplicates-all.txt"
-	echo "Completed at $(date)"
-
 }
 
+search() {
+
+	fdupes -r $targetdir > $resultsfile
+	echo "Completed at $(date)"
+}
+
+search-omitted() {
+
+	fdupes -rf $targetdir > $resultsfileomitted
+	echo "Completed at $(date)"
+}
+
+#escapingresults1() {
+	#sed -i  $resultsfile
+	#echo "Completed at $(date)"
+#}
+
+
+# sed Replaced with IFS
+## sed 
+#cat duplicates-confidential-omitfirst-dup-match.txt |sed 's/\ /\\ /g;s/(/\\(/g;s/)/\\)/g' > duplicates-confidential-omitfirst-dup-match2.txt
+#
+## check paths 
+#for i in $(cat duplicates-confidential-omitfirst-dup-match2.txt); do ls $i; done
+#
+#
+
+#If you find the < <(command) syntax unfamiliar you should read about process substitution. 
+#The advantage of this over for file in $(find ...) is that files with spaces, newlines and other characters are correctly handled. 
+#This works because find with -print0 will use a null (aka \0) as the terminator for each file name and, unlike newline, null is not a 
+#legal character in a file name.
+
+
+removespaces() {
+
+    while IFS= read -r line; do
+	    printf '%q\n' "$line"
+    done < $resultsfile
+}
+
+# executing
+createresults
 search
+search-ommited
+removespaces
+
 exit 0
